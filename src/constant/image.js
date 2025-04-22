1d3bcd7c142271b8ac268195fc66cadf4fa59f2a
aaryae <aaryaed@gmail.com>
2025-04-22 16:25:34 +0545

    config: optimization for image:easy image import

---
+++ src/constant/image.js
@@ -0,0 +1,23 @@
+import aarya from "../assets/aarya1.jpg";
+import crown from "../assets/crown.png";
+import food from "../assets/food.png";
+import mandala from "../assets/mandala.png";
+import news from "../assets/news.png";
+import saloon from "../assets/saloon.png";
+import site from "../assets/site.png";
+import weather from "../assets/weather.png";
+import eco from "../assets/eco.png";
+
+const image = {
+  news,
+  mandala,
+  saloon,
+  site,
+  weather,
+  aarya,
+  crown,
+  food, 
+  eco,
+};
+
+export default image;
