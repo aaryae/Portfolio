027777fa6bad59e4abc8de5b8dac7cfa494276c7
aaryae <aaryaed@gmail.com>
2025-04-22 15:32:50 +0545

    feat: navbar edit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,17 @@ export default function Navbar() {
   return (
     <nav className="bg-white text-[#000000af] w-full fixed top-0 z-40 shadow-md">
       <div className="max-w-[1500px] mx-auto w-full flex gap-8 items-center justify-between py-4 px-5">
-        <h1 className="text-6xl uppercase cursor-pointer text-black font-bold">
+        <h1 className="text-6xl uppercase cursor-pointer text-black font-bold relative">
           AARYA
+          {/* <img
+            src={mandala}
+            className="absolute top-0 slowspin -z-10"
+            width={300}
+            alt=""
+          /> */}
         </h1>
         <button id="menu-toggle" className="md:hidden text-2xl">
-          &#9776;
+          &#9776;import mandala from "../assets/mandala.png";
         </button>
         <div className="hidden md:flex">
           <ul className="flex gap-7">
