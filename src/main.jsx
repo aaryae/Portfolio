f5bea9e7987fcef36dc884cd9b011935b23bfa68
aaryae <aaryaed@gmail.com>
2026-07-04 23:00:32 +0545

    fix: rever to old favicon

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,9 @@ import "aos/dist/aos.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { startAnimatedFavicon } from "./utils/animatedFavicon";
 import "./index.css";
 
 AOS.init();
-startAnimatedFavicon();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
